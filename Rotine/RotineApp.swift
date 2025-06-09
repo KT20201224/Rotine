3f8b3f4a70265f161a9818df71d6dbb82ee1f047
KT20201224 <rudxo0525@gmail.com>
2025-06-09 21:15:42 +0900

    Initial Commit

---
+++ Rotine/RotineApp.swift
@@ -0,0 +1,17 @@
+//
+//  RotineApp.swift
+//  Rotine
+//
+//  Created by 김경태 on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct RotineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RotineTests/RotineTests.swift
@@ -0,0 +1,17 @@
+//
+//  RotineTests.swift
+//  RotineTests
+//
+//  Created by 김경태 on 6/9/25.
+//
+
+import Testing
+@testable import Rotine
+
+struct RotineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
